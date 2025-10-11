d87a341b7a2053685374c8b25ea28df3729d0fb7
Adel Prokurov <adel.prokurov281@outlook.com>
2025-10-11 12:20:40 +0400

    fix print

--- boot/print.scm
+++ boot/print.scm
@@ -4,7 +4,7 @@
            
             (print 
                 (string-append "#<procedure" (let ([name (procedure-name proc)])
-                    (if name (string-append " " (symbol->string name)) "")) " " (number->string (hash proc) 16) ">")
+                    (if name (string-append " " (if (symbol? name) (symbol->string name) name)) "")) " " (number->string (hash proc) 16) ">")
                 port 
                 #f))))
 
