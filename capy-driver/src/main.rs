8759287d0f6c9f799635424cbf324001afb471a7
Adel Prokurov <adel.prokurov281@outlook.com>
2025-10-13 20:56:08 +0400

    resume execution if GC happened

--- capy-driver/src/main.rs
+++ capy-driver/src/main.rs
@@ -2,7 +2,7 @@ use capy::{
     init_scheme,
     runtime::{
         modules::public_ref,
-        vm::{base::get_program_arguments_fluid, call_scheme},
+        vm::{VMResult, base::get_program_arguments_fluid, call_scheme},
     },
 };
 
@@ -10,10 +10,26 @@ fn main() {
     env_logger::init();
     let scm = init_scheme();
 
-    scm.enter(|ctx| {
+    let mut did_yield = scm.enter(|ctx| {
         let entry = public_ref(ctx, "boot cli", "enter");
 
         let args = get_program_arguments_fluid(ctx);
-        call_scheme(ctx, entry.expect("malformed installation"), [args]);
+        match call_scheme(ctx, entry.expect("malformed installation"), [args]) {
+            VMResult::Yield => true,
+            _ => false,
+        }
     });
+
+    while did_yield {
+        scm.enter(|ctx| {
+            if ctx.has_suspended_call() {
+                match ctx.resume_suspended_call() {
+                    VMResult::Yield => did_yield = true,
+                    _ => did_yield = false,
+                }
+            } else {
+                did_yield = false;
+            }
+        })
+    }
 }
