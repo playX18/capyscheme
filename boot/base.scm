9c10a7be1560f3f42fe8179df82a98d3bb760654
Adel Prokurov <adel.prokurov281@outlook.com>
2025-10-18 17:35:21 +0400

    feat(base): cond-expand

--- boot/base.scm
+++ boot/base.scm
@@ -403,7 +403,7 @@
             [(_ (name name* ...) body ...)
                 #'(begin 
                     (eval-when (expand load eval)
-                        (let ([m (define-module* '(name name* ...))])
+                        (let ([m ((@@ (capy) define-module*) '(name name* ...))])
                             (current-module m)))
                     (@@ @@ (name name* ...) body) ...
                     )])))
@@ -773,29 +773,29 @@
 
 
 (eval-when (expand load eval)
-(define (unwrap stx)
-  (syntax-case stx ()
-    ((a . b) (cons (unwrap #'a) (unwrap #'b)))
-    (#(a ...) (list->vector (map unwrap (vector->list #'(a ...)))))
-    (id (identifier? #'id) #'id)
-    (_ (syntax->datum stx))))
+  (define (unwrap stx)
+    (syntax-case stx ()
+      ((a . b) (cons (unwrap #'a) (unwrap #'b)))
+      (#(a ...) (list->vector (map unwrap (vector->list #'(a ...)))))
+      (id (identifier? #'id) #'id)
+      (_ (syntax->datum stx))))
 
 
-(define (rewrap ctx expr)
-  (let rewrap* ((e expr))
-    (cond
-     ((pair? e) (cons (rewrap* (car e)) (rewrap* (cdr e))))
-     ((vector? e) (vector-map rewrap* e))
-     ((identifier? e) e)
-     (else (datum->syntax ctx e #f)))))
+  (define (rewrap ctx expr)
+    (let rewrap* ((e expr))
+      (cond
+      ((pair? e) (cons (rewrap* (car e)) (rewrap* (cdr e))))
+      ((vector? e) (vector-map rewrap* e))
+      ((identifier? e) e)
+      (else (datum->syntax ctx e #f)))))
 
-(define (make-compare ctx)
-  (lambda (x y)
-    (free-identifier=? (rewrap ctx x) (rewrap ctx y))))
+  (define (make-compare ctx)
+    (lambda (x y)
+      (free-identifier=? (rewrap ctx x) (rewrap ctx y))))
 
-(define (make-rename ctx)
-  (lambda (x)
-    (datum->syntax ctx x #f))))
+  (define (make-rename ctx)
+    (lambda (x)
+      (datum->syntax ctx x (syntax-source ctx)))))
 
 
 (define-syntax er-macro-transformer
@@ -941,7 +941,6 @@
           [(absolute-path-string? file) file]
           [(string? dir) (string-append dir "/" file)]
           [else file]))
-      (format #t "Including file: ~a\n" path)
       (call-with-input-file
         path
         proc)))))
@@ -956,7 +955,7 @@
             (cons #'begin 
               (let lp ()
                 (let ([x (read-syntax port)])
-                  (if (eof-object? (syntax-expression x))
+                  (if (eof-object? x)
                       '()
                       (cons (datum->syntax #'filename x) (lp))))))))])))
 
@@ -1072,6 +1071,130 @@
              e1 ... e2)]
         [_ (syntax-violation 'with-implicit "invalid syntax" x)])))
 
+(define-syntax false-if-exception 
+  (lambda (stx)
+    (syntax-case stx ()
+      [(_ expr)
+       #'(with-exception-handler 
+        (lambda ex #f)
+        (lambda () expr))])))
+
+;;; {`cond-expand' for SRFI-0 support.}
+;;;
+;;; This syntactic form expands into different commands or
+;;; definitions, depending on the features provided by the Scheme
+;;; implementation.
+;;;
+;;; Syntax:
+;;;
+;;; <cond-expand>
+;;;   --> (cond-expand <cond-expand-clause>+)
+;;;     | (cond-expand <cond-expand-clause>* (else <command-or-definition>))
+;;; <cond-expand-clause>
+;;;   --> (<feature-requirement> <command-or-definition>*)
+;;; <feature-requirement>
+;;;   --> <feature-identifier>
+;;;     | (and <feature-requirement>*)
+;;;     | (or <feature-requirement>*)
+;;;     | (not <feature-requirement>)
+;;; <feature-identifier>
+;;;   --> <a symbol which is the name or alias of a SRFI>
+;;;
+;;; Additionally, this implementation provides the
+;;; <feature-identifier>s `guile' and `r5rs', so that programs can
+;;; determine the implementation type and the supported standard.
+;;;
+;;; Remember to update the features list when adding more SRFIs.
+;;;
+
+(define %cond-expand-features
+  ;; This should contain only features that are present in core Guile,
+  ;; before loading any modules.  Modular features are handled by
+  ;; placing 'cond-expand-provide' in the relevant module.
+  '(capy
+    r5rs
+    r6rs
+    r7rs
+    exact-closed ieee-float full-unicode ratios ;; R7RS features.
+    srfi-0   ;; cond-expand itself
+    srfi-4   ;; homogeneous numeric vectors
+    srfi-6   ;; string ports
+    srfi-13  ;; string library
+    srfi-14  ;; character sets
+    srfi-16  ;; case-lambda
+    srfi-23  ;; `error` procedure
+    srfi-30  ;; nested multi-line comments
+    srfi-39  ;; parameterize
+    srfi-46  ;; basic syntax-rules extensions
+    srfi-55  ;; require-extension
+    srfi-61  ;; general cond clause
+    srfi-62  ;; s-expression comments
+    srfi-87  ;; => in case clauses
+    srfi-105 ;; curly infix expressions
+    ))
+
+(set! %cond-expand-features 
+      (cons (string->symbol (host-os)) %cond-expand-features))
+(set! %cond-expand-features 
+      (cons (string->symbol (host-family)) %cond-expand-features))
+(set! %cond-expand-features 
+      (cons (string->symbol (host-arch)) %cond-expand-features))
+
+;; This table maps module public interfaces to the list of features.
+;;
+(define %cond-expand-table (make-core-hash-eq))
+
+;; Add one or more features to the `cond-expand' feature list of the
+;; module `module'.
+;;
+(define (cond-expand-provide module features)
+  (let ((mod (module-public-interface module)))
+    (and mod
+         (core-hash-put! %cond-expand-table mod
+                     (append (core-hash-ref %cond-expand-table mod '())
+                             features)))))
+
+(define-syntax cond-expand
+  (lambda (x)
+    (define (module-has-feature? mod sym)
+      (or-map (lambda (mod)
+                (memq sym (core-hash-ref %cond-expand-table mod '())))
+              (module-uses mod)))
+
+    (define (condition-matches? condition)
+      (syntax-case condition (and or not)
+        ((and c ...)
+         (and-map condition-matches? #'(c ...)))
+        ((or c ...)
+         (or-map condition-matches? #'(c ...)))
+        ((not c)
+         (if (condition-matches? #'c) #f #t))
+        (c
+         (identifier? #'c)
+         (let ((sym (syntax->datum #'c)))
+           (if (memq sym %cond-expand-features)
+               #t
+               (module-has-feature? (current-module) sym))))))
+
+    (define (match clauses alternate)
+      (syntax-case clauses ()
+        (((condition form ...) . rest)
+         (if (condition-matches? #'condition)
+             #'(begin form ...)
+             (match #'rest alternate)))
+        (() (alternate))))
+
+    (syntax-case x (else)
+      ((_ clause ... (else form ...))
+       (match #'(clause ...)
+         (lambda ()
+           #'(begin form ...))))
+      ((_ clause ...)
+       (match #'(clause ...)
+         (lambda ()
+           (syntax-violation 'cond-expand "unfulfilled cond-expand" x)))))))
+
+
 (define-syntax define-library 
   (lambda (stx)
     (define (handle-includes filenames)
@@ -1103,8 +1226,6 @@
               (syntax->datum #'lib-name)))))
           (id
            (identifier? #'id)
-           ;; FIXME: R7RS (features) isn't quite the same as
-           ;; %cond-expand-features; see scheme/base.scm.
            (memq (syntax->datum #'id) %cond-expand-features))))
       (syntax-case clauses (else)
         (() #'())  ; R7RS says this is not specified :-/
