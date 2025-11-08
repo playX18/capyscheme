e19b9b220a5326518786c2a1b4fd3f96eb0793a1
Adel Prokurov <adel.prokurov281@outlook.com>
2025-11-08 11:29:10 +0700

    add boot/control.scm

---
+++ boot/control.scm
@@ -0,0 +1,158 @@
+
+(define (current-continuation-marks)
+  (current-continuation-marks))
+
+
+(define current-exception-handler
+  (let ([f (make-thread-local-fluid #f)])
+    (lambda args
+      (if (null? args)
+          (fluid-ref f)
+          (let ([old (fluid-ref f)])
+            (fluid-set! f (car args))
+            old)))))
+
+(define parent-exception-handler
+  (let ([f (make-thread-local-fluid #f)])
+    (lambda args
+      (if (null? args)
+          (fluid-ref f)
+          (let ([old (fluid-ref f)])
+            (fluid-set! f (car args))
+            old)))))
+
+
+(define current-dynamic-wind-record 
+  (let ([f (make-thread-local-fluid '())])
+    (lambda args
+      (if (null? args)
+          (fluid-ref f)
+          (let ([old (fluid-ref f)])
+            (fluid-set! f (car args))
+            old)))))
+
+(define (perform-dynamic-wind new cont args)
+  (define common-tail
+      (lambda (x y)
+        (let ((nx (length x)) (ny (length y)))
+          (do
+            ((x (if (> nx ny) (list-tail x (- nx ny)) x) (cdr x))
+             (y (if (> ny nx) (list-tail y (- ny nx)) y) (cdr y)))
+            ((eq? x y) x)))))
+    (let ((tail (common-tail new ($winders))))
+      (let loop ((rec ($winders)))
+        (cond ((not (eq? rec tail)) ($winders (cdr rec)) ((cdar rec)) (loop (cdr rec)))))
+      (let loop ((rec new))
+        (cond ((not (eq? rec tail)) (loop (cdr rec)) ((caar rec)) ($winders rec)))))
+    (apply cont args))
+
+(define (dynamic-wind in body out)
+  (in)
+  ($winders (cons (cons in out) ($winders)))
+  (call-with-values 
+    body
+    (lambda ans 
+      ($winders (cdr ($winders)))
+      (out)
+      (apply values ans))))
+
+(define (call/cc f)
+  (define (attach-cont-props winders attachments k)
+    (set-procedure-property! k 'continuation
+      (tuple winders attachments))
+    k)
+
+  (define saved-record ($winders))
+  (define marks (current-continuation-marks))
+  (.call/cc-unsafe 
+    (lambda (k)
+      (f (attach-cont-props 
+        saved-record
+        marks
+        (lambda args 
+          ($set-attachments! marks)
+          (perform-dynamic-wind saved-record k args)))))))
+
+(define call-with-current-continuation call/cc)
+
+(define (continuation? x)
+  (and (procedure? x)
+       (if (procedure-property x 'continuation)
+         #t
+         #f)))
+
+(define (continuation-next-marks k)
+  (unless (continuation? k)
+    (assertion-violation 'continuation-marks "expected a continuation" k))
+  (let ([props (procedure-property k 'continuation)])
+    (tuple-ref props 1)))
+  
+(define (call-in-continuation c proc . args)
+    (define (cont-attachments c)
+        (tuple-ref (cdr (procedure-property c 'continuation)) 1))
+    (unless (continuation? c)
+        (error 'call-in-continuation "not a continuation" c))
+
+    (cond 
+        [(null? args)
+            ($set-attachments! (cont-attachments c))
+            (receive vals (proc)
+                (apply c vals))]
+        [else 
+            (let ([set proc]
+                  [proc (car args)])
+                (unless (continuation-marks? set)
+                    (error 'call-in-continuation 
+                        "expected continuation-marks object" set))
+                (let* ([markss ($continuation-marks-markss set)]
+                       [c-marks (cont-attachments c)])
+                    (unless (or (eq? markss c-marks)
+                                (and (pair? markss) (eq? (cdr markss) c-markss)))
+                        (error 'call-in-continuation "mark set is not an extension of current marks" set c)))
+                    ($set-attachments! markss)
+                    (receive vals (proc)
+                        (apply c vals))) ]))
+
+(define $null-continuation 
+  (let ([k (lambda () (exit 1))])
+    (set-procedure-property! k 'continuation
+      (tuple '() '()))
+    k 
+  ))
+
+
+(define (unhandled-exception-error val)
+  (.return-error val))
+
+
+(define *basic-exception-handlers* (list unhandled-exception-error))
+
+(define *current-exception-handlers*
+  (let ([f (make-thread-local-fluid *basic-exception-handlers*)])
+    (lambda args
+      (if (null? args)
+          (fluid-ref f)
+          (let ([old (fluid-ref f)])
+            (fluid-set! f (car args))
+            old)))))
+
+
+(define (raise obj)
+  (.raise obj))
+
+(define (raise-continuable obj)
+  (let ((handlers (*current-exception-handlers*)))
+    (with-exception-handler (cadr handlers)
+      (lambda () ((car handlers) obj)))))
+
+(define (with-exception-handler handler thunk)
+  (let* ([previous-handlers (*current-exception-handlers*)]
+         [new-handlers (if handler
+                          (cons handler previous-handlers)
+                          previous-handlers)])
+    (dynamic-wind
+      (lambda ()
+        (*current-exception-handlers* new-handlers))
+      (lambda() (.with-handler handler thunk))
+      (lambda ()
+        (*current-exception-handlers* previous-handlers)))))
--- boot/main.scm
+++ boot/main.scm
@@ -402,174 +402,22 @@
 (define *log-level* 0)
 
 
-(define (current-continuation-marks)
-  (current-continuation-marks))
 
+(define (procedure-property proc key)
+  (cond 
+    [(procedure-properties proc)
+      (let ([p (assq key (procedure-properties proc))])
+        (and p (cdr p)))]
+    [else #f]))
 
-(define current-exception-handler
-  (let ([f (make-thread-local-fluid #f)])
-    (lambda args
-      (if (null? args)
-          (fluid-ref f)
-          (let ([old (fluid-ref f)])
-            (fluid-set! f (car args))
-            old)))))
-
-(define parent-exception-handler
-  (let ([f (make-thread-local-fluid #f)])
-    (lambda args
-      (if (null? args)
-          (fluid-ref f)
-          (let ([old (fluid-ref f)])
-            (fluid-set! f (car args))
-            old)))))
-
-
-(define current-dynamic-wind-record 
-  (let ([f (make-thread-local-fluid '())])
-    (lambda args
-      (if (null? args)
-          (fluid-ref f)
-          (let ([old (fluid-ref f)])
-            (fluid-set! f (car args))
-            old)))))
-
-(define (perform-dynamic-wind new cont args)
-  (define common-tail
-      (lambda (x y)
-        (let ((nx (length x)) (ny (length y)))
-          (do
-            ((x (if (> nx ny) (list-tail x (- nx ny)) x) (cdr x))
-             (y (if (> ny nx) (list-tail y (- ny nx)) y) (cdr y)))
-            ((eq? x y) x)))))
-    (let ((tail (common-tail new ($winders))))
-      (let loop ((rec ($winders)))
-        (cond ((not (eq? rec tail)) ($winders (cdr rec)) ((cdar rec)) (loop (cdr rec)))))
-      (let loop ((rec new))
-        (cond ((not (eq? rec tail)) (loop (cdr rec)) ((caar rec)) ($winders rec)))))
-    (apply cont args))
-
-(define (dynamic-wind in body out)
-  (in)
-  ($winders (cons (cons in out) ($winders)))
-  (call-with-values 
-    body
-    (lambda ans 
-      ($winders (cdr ($winders)))
-      (out)
-      (apply values ans))))
-
-(define (call/cc f)
-  (define (attach-cont-props winders attachments k)
-    (set-procedure-property! k 'continuation
-      (tuple winders attachments))
-    k)
-
-  (define saved-record ($winders))
-  (define marks (current-continuation-marks))
-  (.call/cc-unsafe 
-    (lambda (k)
-      (f (attach-cont-props 
-        saved-record
-        marks
-        (lambda args 
-          ($set-attachments! marks)
-          (perform-dynamic-wind saved-record k args)))))))
-
-(define call-with-current-continuation call/cc)
-
-(define (continuation? x)
-  (and (procedure? x)
-       (if (procedure-property x 'continuation)
-         #t
-         #f)))
-
-(define (continuation-next-marks k)
-  (unless (continuation? k)
-    (assertion-violation 'continuation-marks "expected a continuation" k))
-  (let ([props (procedure-property k 'continuation)])
-    (tuple-ref props 1)))
-  
-(define (call-in-continuation c proc . args)
-    (define (cont-attachments c)
-        (tuple-ref (cdr (procedure-property c 'continuation)) 1))
-    (unless (continuation? c)
-        (error 'call-in-continuation "not a continuation" c))
-
-    (cond 
-        [(null? args)
-            ($set-attachments! (cont-attachments c))
-            (receive vals (proc)
-                (apply c vals))]
-        [else 
-            (let ([set proc]
-                  [proc (car args)])
-                (unless (continuation-marks? set)
-                    (error 'call-in-continuation 
-                        "expected continuation-marks object" set))
-                (let* ([markss ($continuation-marks-markss set)]
-                       [c-marks (cont-attachments c)])
-                    (unless (or (eq? markss c-marks)
-                                (and (pair? markss) (eq? (cdr markss) c-markss)))
-                        (error 'call-in-continuation "mark set is not an extension of current marks" set c)))
-                    ($set-attachments! markss)
-                    (receive vals (proc)
-                        (apply c vals))) ]))
-
-(define $null-continuation 
-  (let ([k (lambda () (exit 1))])
-    (set-procedure-property! k 'continuation
-      (tuple '() '()))
-    k 
-  ))
-
-
-(define (unhandled-exception-error val)
-  (.return-error val))
-
-
-(define *basic-exception-handlers* (list unhandled-exception-error))
-
-(define *current-exception-handlers*
-  (let ([f (make-thread-local-fluid *basic-exception-handlers*)])
-    (lambda args
-      (if (null? args)
-          (fluid-ref f)
-          (let ([old (fluid-ref f)])
-            (fluid-set! f (car args))
-            old)))))
-
-
-(define (raise obj)
-  (.raise obj))
-
-(define (raise-continuable obj)
-  (let ((handlers (*current-exception-handlers*)))
-    (with-exception-handler (cadr handlers)
-      (lambda () ((car handlers) obj)))))
-
-(define (with-exception-handler handler thunk)
-  (let* ([previous-handlers (*current-exception-handlers*)]
-         [new-handlers (if handler
-                          (cons handler previous-handlers)
-                          previous-handlers)])
-    (dynamic-wind
-      (lambda ()
-        (*current-exception-handlers* new-handlers))
-      (lambda() (.with-handler handler thunk))
-      (lambda ()
-        (*current-exception-handlers* previous-handlers)))))
-
-
-
-(define (filter pred lst)
-  (let loop ([lst lst] [acc '()])
-    (if (null? lst)
-      (reverse acc)
-      (if (pred (car lst))
-        (loop (cdr lst) (cons (car lst) acc))
-        (loop (cdr lst) acc)))))
-
+(define (set-procedure-property! proc key value)
+  (let ([props (procedure-properties proc)])
+    (if props
+      (let ([p (assq key props)])
+        (if p
+          (set-cdr! p value)
+          (set-procedure-properties! proc (cons (cons key value) props))))
+      (set-procedure-properties! proc (list (cons key value))))))
 
 ;; These functions are defined in `boot/eval.scm`. Due to the fact that
 ;; we now have letrectification overriding them also requires overriding
@@ -590,6 +438,21 @@
   "Loads file by searching only load path or by its absolute path."
   (let ([thunk (load-thunk-in-vicinity filename #f)])
     (thunk)))
+
+
+(primitive-load "boot/control.scm")
+
+
+
+(define (filter pred lst)
+  (let loop ([lst lst] [acc '()])
+    (if (null? lst)
+      (reverse acc)
+      (if (pred (car lst))
+        (loop (cdr lst) (cons (car lst) acc))
+        (loop (cdr lst) acc)))))
+
+
 
 (define (module-search fn m v)
   (or (fn m v)
@@ -1097,21 +960,6 @@
       (apply assertion-violation 'assert "assertion failed" rest)
       #t))
 
-(define (procedure-property proc key)
-  (cond 
-    [(procedure-properties proc)
-      (let ([p (assq key (procedure-properties proc))])
-        (and p (cdr p)))]
-    [else #f]))
-
-(define (set-procedure-property! proc key value)
-  (let ([props (procedure-properties proc)])
-    (if props
-      (let ([p (assq key props)])
-        (if p
-          (set-cdr! p value)
-          (set-procedure-properties! proc (cons (cons key value) props))))
-      (set-procedure-properties! proc (list (cons key value))))))
 
 (define (procedure-name proc)
   (procedure-property proc 'name))
