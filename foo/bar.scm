b1b0f0a3b530fdce99ef5d178d4862991c7dd717
playX18 <adel.prokurov281@outlook.com>
2025-12-06 19:02:47 +0700

    remove unused files

--- foo/bar.scm
+++
@@ -1,7 +0,0 @@
-
-(define (hello)
-  (display "Hello world")
-  (newline))
-
-(printf "foo/bar.scm loaded, current-module: ~a~%" (current-module))
-(printf "hello=~a~%" hello)
--- foo/bar.sld
+++
@@ -1,8 +0,0 @@
-
-
-(define-library
-  (foo bar)
-  (import (scheme base)
-          (scheme write) (capy))
-  (export hello)
-  (include "bar.scm"))
