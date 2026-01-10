149a4d35b08c931c5c35763f928aa1012a88a2db
playX18 <adel.prokurov281@outlook.com>
2026-01-10 11:42:51 +0700

    feat(runtime): add Socket type

--- capy/src/runtime/value/port.rs
+++ capy/src/runtime/value/port.rs
@@ -1,1 +1,39 @@
+use crate::{
+    Trace,
+    prelude::{ScmHeader, Tagged, TypeCode8},
+};
 
+#[repr(C)]
+pub struct Socket {
+    pub(crate) header: ScmHeader,
+    pub(crate) addr: libc::sockaddr_storage,
+    pub(crate) fd: i32,
+    pub(crate) mode: SocketMode,
+    pub(crate) family: i32,
+    pub(crate) socktype: i32,
+    pub(crate) protocol: i32,
+    pub(crate) addrlen: i32,
+}
+
+unsafe impl Trace for Socket {
+    unsafe fn trace(&mut self, visitor: &mut crate::Visitor) {
+        let _ = visitor;
+    }
+
+    unsafe fn process_weak_refs(&mut self, weak_processor: &mut crate::WeakProcessor) {
+        let _ = weak_processor;
+    }
+}
+
+unsafe impl Tagged for Socket {
+    const TYPE_NAME: &'static str = "socket";
+    const TC8: super::TypeCode8 = TypeCode8::SOCKET;
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+
+pub enum SocketMode {
+    None,
+    Server,
+    Client,
+}
