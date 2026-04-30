e551a911a1704e6744ff456c2491a27dfa2d1742
playX18 <adel.prokurov281@outlook.com>
2026-04-30 20:35:10 +0700

    fix(lib/core): mutex protect data

--- lib/core/threading.scm
+++ lib/core/threading.scm
@@ -20,24 +20,26 @@
     terminated-thread-exception?
     terminated-thread-exception-thread)
   (import (core primitives)
-          (core hashtables)
-          (core conditions)
-          (core records))
+    (core hashtables)
+    (core conditions)
+    (core records)
+    (only (capy) printf))
 
-  (define-condition-type &uncaught-exception &error 
+  (define-condition-type &uncaught-exception &error
     make-uncaught-exception
     uncaught-exception?
     (reason uncaught-exception-reason))
 
-  (define-condition-type terminated-thread-exception &error 
+  (define-condition-type terminated-thread-exception &error
     make-terminated-thread-exception
     terminated-thread-exception?
     (thread terminated-thread-exception-thread))
 
   (define-record-type uncaught
-    (fields 
+    (fields
       reason))
 
+  (define %thread-data-mutex (make-mutex))
   ;; weak hashtable which keeps thread data until thread object is alive
   (define %thread-results (make-weak-hashtable))
   (define %thread-join-data (make-weak-hashtable))
@@ -49,25 +51,29 @@
 
     (let ([t (fork-thread
               (lambda ()
-                (call/cc 
+                (call/cc
                   (lambda (return)
                     (call-with-values
                       (lambda ()
+
                         (with-mutex mutex
                           (set! thread (current-thread))
-                          (hashtable-set! %thread-join-data thread (cons cv mutex))
+                          (with-mutex %thread-data-mutex
+                            (hashtable-set! %thread-join-data thread (cons cv mutex)))
                           (condition-signal cv))
                         (with-exception-handler
                           (lambda (exn)
                             (define obj (make-uncaught exn))
-                            (with-mutex mutex 
-                              (hashtable-set! %thread-results cv obj)
+                            (with-mutex mutex
+                              (with-mutex %thread-data-mutex
+                                (hashtable-set! %thread-results cv obj))
                               (condition-broadcast cv))
                             (return #f))
                           thunk))
                       (lambda vals
                         (with-mutex mutex
-                          (hashtable-set! %thread-results cv vals)
+                          (with-mutex %thread-data-mutex
+                            (hashtable-set! %thread-results cv vals))
                           (condition-broadcast cv))))))))])
 
       (with-mutex mutex
